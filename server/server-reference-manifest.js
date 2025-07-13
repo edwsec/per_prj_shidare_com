d0acf426aa4383999f8b639e14a995b856f975d8
edwsec <edwsec@shidare.com>
2025-07-13 21:14:23 +0200

    Added new website developed using vibe coding

--- server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GtY/nS4uI/pS+4RMXAmfx+r6AGJgPFGTcwvVYRdiLjs=\"\n}"
--- static/chunks/src_app_layout_tsx_007ca514._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__8ebb6d4b._.css",
-  "static/chunks/node_modules_next_9e6a8075._.js",
-  "static/chunks/src_components_Sidebar_tsx_e04ac0a0._.js"
-],
-    source: "dynamic"
-});
